75253e2926f8c4443a58d19b32cc78897d9dcaa5
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2023-12-17 15:54:42 -0300

    feat: Adicionado tabela workouts

---
+++ database/migrations/2023_12_17_184912_create_workouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workouts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('exercise_id');
+            $table->integer('repetitions');
+            $table->decimal('weight');
+            $table->integer('break_time');
+            $table->enum('day', ['SEGUNDA', 'TERÇA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'DOMINGO']);
+            $table->text('observations')->nullable();
+            $table->integer('time');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('exercise_id')->references('id')->on('exercises');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workouts');
+    }
+};
