a918cfa2dc978c9f262515e655e2033f3a91805b
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-08 11:45:16 -0300

    fix: Ajusta atributos do UserModel

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array<int, string>
      */
-    protected $fillable = ['name', 'email', 'password', 'date_birth', 'cpf', 'plan_id'];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
