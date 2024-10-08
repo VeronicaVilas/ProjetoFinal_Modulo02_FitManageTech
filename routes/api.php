4bf5457ce6462a9d638a8652a9b191dc1a8e6fa5
Verônica Vilas <veronica_v_santos@estudante.sesisenai.org.br>
2024-10-08 11:46:05 -0300

    feat: cria migração de tabela de transações

--- routes/api.php
+++ routes/api.php
@@ -1,37 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ExerciseController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\StudentExportController;
-use App\Http\Controllers\StudentWorkoutController;
+use App\Http\Controllers\FinancialSummaryController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\WorkoutController;
-use App\Http\Middleware\CheckStudentPlanLimitToUser;
+
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('api')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::get('students/export', [StudentExportController::class, 'export']);
+    Route::post('transacoes', [TransactionController::class, 'store']);
+    Route::get('transacoes', [TransactionController::class, 'index']);
+    Route::get('transacoes/{id}', [TransactionController::class, 'show']);
+    Route::put('transacoes/{id}', [TransactionController::class, 'update']);
+    Route::delete('transacoes/{id}', [TransactionController::class, 'destroy']);
 
-    Route::get('dashboard', [DashboardController::class, 'index']);
-
-    Route::post('exercises', [ExerciseController::class, 'store']);
-    Route::get('exercises', [ExerciseController::class, 'index']);
-    Route::delete('exercises/{id}', [ExerciseController::class, 'destroy']);
-
-    Route::post('students', [StudentController::class, 'store'])->middleware(CheckStudentPlanLimitToUser::class);
-    Route::get('students', [StudentController::class, 'index']);
-    Route::delete('students/{id}', [StudentController::class, 'destroy']);
-    Route::put('students/{id}', [StudentController::class, 'update']);
-    Route::get('students/{id}', [StudentController::class, 'show']);
-
-    Route::post('workouts', [WorkoutController::class, 'store']);
-
-    Route::get('students/{id}/workouts', [StudentWorkoutController::class, 'index']);
+    Route::get('resumo', [FinancialSummaryController::class, 'getSummary']);
 });
 
 Route::post('users', [UserController::class, 'store']);
-Route::post('login', [AuthController::class, 'store']);
+Route::post('login', [AuthController::class, 'login']);
